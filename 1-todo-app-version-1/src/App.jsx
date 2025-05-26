b6f680c5540a44408df761d182680bde07e8def3
amitkvCode <amitkv180@gmail.com>
2025-05-27 00:36:04 +0530

    Update to TodoApp 3

--- 1-todo-app-version-1/src/App.jsx
+++ 1-todo-app-version-1/src/App.jsx
@@ -7,11 +7,13 @@ import "./App.css";
  
 function App() {
   return (
-    <center class="todo-container">
+    <center className="todo-container">
       <AppName />
       <AddTodo />
+      <div className="items-container">
       <TodoItem1></TodoItem1>
       <TodoItem2></TodoItem2>
+      </div>
     </center>
   );
 }
--- 1-todo-app-version-1/src/components/TodoItem2.jsx
+++ 1-todo-app-version-1/src/components/TodoItem2.jsx
@@ -2,12 +2,12 @@ function TodoItem2() {
   let todoName = 'Go to Collage';
   let todoDate = '26/10/2023';
   return (
-    <div class="container text-center">
-      <div class="row">
+    <div class="container">
+      <div class="row my-row">
         <div class="col-6">{todoName}</div>
         <div class="col-4">{todoDate}</div>
         <div class="col-2">
-          <button type="button" class="btn btn-danger">
+          <button type="button" class="btn btn-danger my-button">
             Delete
           </button>
         </div>
