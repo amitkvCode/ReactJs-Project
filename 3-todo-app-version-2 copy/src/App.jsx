63721cbd1f05eb32a47a90694eb06d56bf395519
amitkvCode <amitkv180@gmail.com>
2025-06-04 19:16:03 +0530

    Update

--- 3-todo-app-version-2 copy/src/App.jsx
+++ 3-todo-app-version-2 copy/src/App.jsx
@@ -5,37 +5,35 @@ import Container from "./components/container";
 
 import "./App.css";
 
- 
 function App() {
   // Array of object used to store data;
- const todoItems = [
-  {
-    name: "Buy Milk",
-    dueDate: "26/10/2023",
-
-  },
-  {
-    name: "Go to Collage",
-    dueDate: "26/10/2024",
-  },
-  {
-    name: "Go Now",
-    dueDate: "26/10/2025",
-  },
- ];
-  return ( <div>
-    <Container>
-    <center className="todo-container">
-      <AppName />
-      <AddTodo /> 
-      <TodoItems todoData={todoItems}></TodoItems> 
-    </center>
-    </Container>
-    <Container>
-      <p>This for you to work for your dream.</p>
-    </Container>
+  const todoItems = [
+    {
+      name: "Buy Milk",
+      dueDate: "26/10/2023",
+    },
+    {
+      name: "Go to Collage",
+      dueDate: "26/10/2024",
+    },
+    {
+      name: "Go Now",
+      dueDate: "26/10/2025",
+    },
+  ];
+  return (
+    <div>
+      <Container>
+        <center className="todo-container">
+          <AppName />
+          <AddTodo />
+          <TodoItems todoData={todoItems}></TodoItems>
+        </center>
+      </Container>
+      <Container>
+        <p>This for you to work for your dream.</p>
+      </Container>
     </div>
   );
 }
-
 export default App;
